ead1047d8aae7349a50c422f86dbd772b5a3919f
Adarsh Chithran <adarsh.chithran@paytm.com>
2016-01-20 19:20:38 +0530

    pushing repository

---
+++ app/src/main/java/hackathon/paytm/hackathonwatsapp/MainActivity.java
@@ -0,0 +1,76 @@
+package hackathon.paytm.hackathonwatsapp;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    ImageView image;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image = (ImageView) findViewById(R.id.image);
+        image.setImageResource(R.drawable.poster3);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        //toolbar.showOverflowMenu();
+
+
+        setSupportActionBar(toolbar);
+
+        Drawable dr = getResources().getDrawable(R.drawable.arrow_white);
+        Bitmap bitmap = ((BitmapDrawable) dr).getBitmap();
+        Drawable d = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap, 90, 50, true));
+        toolbar.setNavigationIcon(d);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("Adarsh Chithran");
+
+        //collapsingToolbarLayout.set
+
+
+        setPalette();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    private void setPalette() {
+        Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(Palette palette) {
+                int primaryDark = getResources().getColor(R.color.primary_dark);
+                int primary = getResources().getColor(R.color.primary);
+                collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(primary));
+                collapsingToolbarLayout.setStatusBarScrimColor(palette.getDarkVibrantColor(primaryDark));
+                //  collapsingToolbarLayout.
+
+            }
+        });
+
+    }
+
+}
+
